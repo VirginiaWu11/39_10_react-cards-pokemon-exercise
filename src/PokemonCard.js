1490a7c8b64d31c9b25fe2f94c8f2a85a3005876
Virginia Wu <virginiawu11@gmail.com>
2021-10-28 21:27:53 -0400

    fix template literals

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -5,17 +5,17 @@ import "./PokemonCard.css";
 function PokemonCard({ front, back, name, stats }) {
   const [isFacingUp, setIsFacingUp] = useState(true);
   const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
+    setIsFacingUp((isUp) => !isUp);
   };
   return (
     <div onClick={flipCard} className="PokemonCard Card">
       {isFacingUp ? (
         <div className="PokemonCard-front">
-          <img src={front} alt={`{name} front`} />
+          <img src={front} alt={`${name} front`} />
           <div>
             <p className="PokemonCard-name">{name}</p>
             <ul className="PokemonCard-stats">
-              {stats.map(stat => (
+              {stats.map((stat) => (
                 <li key={stat.name}>
                   <em>{stat.name}</em>: {stat.value}
                 </li>
@@ -25,7 +25,7 @@ function PokemonCard({ front, back, name, stats }) {
         </div>
       ) : (
         <div className="PokemonCard-back">
-          <img src={back} alt={`{name} back`} />
+          <img src={back} alt={`${name} back`} />
         </div>
       )}
     </div>
